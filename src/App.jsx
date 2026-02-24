7baaa2d1e2c92d978bc144166aef9a62bcc0f3f5
Harry Chopra <22281143+harrychopra@users.noreply.github.com>
2026-02-24 18:06:00 +0000

    Render header with menu, logo and user avatar placeholder

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import './App.css';
+
 export default function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
+    <div className="container">
+      <header>
+        <div className="menu-logo">
+          <div className="menu">&#8801;</div>
+          <div className="logo">Subnet</div>
+        </div>
+        <img className="avatar" src="../assets/avatar.png" alt="avatar" />
+      </header>
     </div>
   );
 }
